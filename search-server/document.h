0a59c29e0da2380d97c9c009ea2eaee88af82670
1abobik1 <125189134+1abobik1@users.noreply.github.com>
2023-11-02 20:09:32 +0300

    Update document.h

--- search-server/document.h
+++ search-server/document.h
@@ -19,3 +19,16 @@ enum class DocumentStatus {
     BANNED,
     REMOVED,
 };
+
+std::ostream& operator<<(std::ostream& out, const Document& document) { 
+
+    out << "{ document_id = "s << document.id 
+
+        << ", relevance = "s << document.relevance 
+
+        << ", rating = "s << document.rating << " }"s; 
+
+    return out; 
+
+} 
+
